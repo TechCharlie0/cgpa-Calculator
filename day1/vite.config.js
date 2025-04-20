f166b23fee84a886f00d5a5003f97c3440b21cb6
isha kandel <ishachttri@gmail.com>
2025-04-20 23:18:53 +0545

    recorrecting

--- day1/vite.config.js
+++ day1/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cgpa-Calculator/day1/", // 👈 important!
+  base: "/cgpa-Calculator/",
 });
